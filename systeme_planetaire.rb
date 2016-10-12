8ec16d1d76fa9c99e456164498028f3fbf4077aa
thepape222 <g.collin@hotmail.com>
2016-10-12 02:40:53 -0400

    Clean avec calcule_force SAUF cyc

--- systeme_planetaire.rb
+++ systeme_planetaire.rb
@@ -28,12 +28,12 @@ class SystemePlanetaire
   #
 
   MODES = [:seq,
-           :par_fj_fin,
-           :par_fj_adj,
-           :par_fj_cyc,
-           :par_sta,
-           :par_dyn,
-          ]
+    :par_fj_fin,
+    :par_fj_adj,
+    :par_fj_cyc,
+    :par_sta,
+    :par_dyn,
+  ]
 
   MODES_AVEC_TAILLE_TACHE = [:par_fj_cyc, :par_sta, :par_dyn]
 
@@ -248,25 +248,40 @@ class SystemePlanetaire
 
 
   def calculer_forces_seq
-      planetes.map { |planete| calcule_force_planet(planete) }
+    calculer_forces_par_fj_adj_ij(0, planetes.size-1)
+  #  planetes.map { |planete| calcule_force_planet(planete) }
   end
 
   def calcule_force_planet (planete)
-      vect = Vector[0, 0] #preduce help
-      planetes.each { |autre| vect += autre.force_de(planete) unless autre.equal?(planete)}
-      vect
+    vect = Vector[0, 0] #preduce help
+    planetes.each { |autre| vect += autre.force_de(planete) unless autre.equal?(planete)}
+    vect
+    #planetes
+  #    .map { |autre| autre.force_de(planete) unless autre.equal?(planete)}
+  #    .reduce (:+)
   end
 
   def calculer_forces_par_fj_fin
     futures = planetes.map { |planete| PRuby.future { calcule_force_planet(planete)} }
-      futures.map(&:value)
+    futures.map(&:value)
   end
 
   def calculer_forces_par_fj_adj
-    # A REMPLACER PAR LA VERSION PARALLELE.
-    calculer_forces_seq
+    nb_threads = [PRuby.nb_threads || planetes.size, planetes.size].min
+    futures = (0...nb_threads).map do |k|
+      PRuby.future do
+        bornes = bornes_tranche( k, nb_threads )
+        calculer_forces_par_fj_adj_ij( bornes.begin, bornes.end)
+      end
+    end
+    futures
+      .map(&:value)
+      .reduce (:+)
   end
 
+  def calculer_forces_par_fj_adj_ij (i,j)
+    (i..j).map { |index| calcule_force_planet(planetes[index]) }
+  end
 
   def calculer_forces_par_fj_cyc
     # A REMPLACER PAR LA VERSION PARALLELE.
@@ -274,18 +289,18 @@ class SystemePlanetaire
   end
 
   def calculer_forces_par_sta
-    # A REMPLACER PAR LA VERSION PARALLELE.
-    calculer_forces_seq
+  #  puts "la valeur de taille_tache: #{taille_tache}" if taille_tache == true
+    planetes.pmap(static:taille_tache) { |planete| calcule_force_planet(planete) }
   end
 
   def calculer_forces_par_dyn
     # A REMPLACER PAR LA VERSION PARALLELE.
-    calculer_forces_seq
+    planetes.pmap(dynamic:taille_tache) { |planete| calcule_force_planet(planete) }
   end
 
- def bornes_tranche( k, nb_threads )
+  def bornes_tranche( k, nb_threads )
     (k * planetes.size / nb_threads..(k + 1) * planetes.size / nb_threads - 1)
- end
+  end
 
 
   #################################################################
@@ -319,14 +334,14 @@ class SystemePlanetaire
       PRuby.future do
         bornes = bornes_tranche( k, nb_threads )
         deplacer_par_fj_adj_ij( bornes.begin, bornes.end, forces, dt )
-       end
-     end
+      end
+    end
     futures.map(&:value)
   end
 
-   def deplacer_par_fj_adj_ij( i, j, forces, dt )
-     (i..j).each { |index| planetes[index].deplacer( forces[index], dt ) unless forces[index].nil? }
-   end
+  def deplacer_par_fj_adj_ij( i, j, forces, dt )
+    (i..j).each { |index| planetes[index].deplacer( forces[index], dt ) unless forces[index].nil? }
+  end
 
   def deplacer_par_fj_cyc( forces, dt )
     # A REMPLACER PAR LA VERSION PARALLELE.
