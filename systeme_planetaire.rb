3d331e9aaad4ca1af7b15f245888ab1d3a2b37b7
thepape222 <thepape222@gmail.com>
2016-10-13 16:36:52 -0400

    cyclique tentative

--- systeme_planetaire.rb
+++ systeme_planetaire.rb
@@ -280,9 +280,7 @@ class SystemePlanetaire
       PRuby.future do
         bornes = bornes_tranche_taille( k, nb_threads, taille_tache )
         puts "la liste de range: #{bornes} pour le thread #{k}"
-        var = 0
-        bornes.each { |borne| var+= calculer_forces_par_fj_adj_ij( borne.begin, borne.end) } unless bornes.nil? || bornes.empty?
-        var
+        var =  (bornes.nil? || bornes.empty?) ? 0 : bornes.reduce(0) { |force ,borne| force + calculer_forces_par_fj_adj_ij( borne.begin, borne.end) } unless bornes.nil?
       end
     end
     futures
